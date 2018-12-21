9bdb4bf09be07a3b3a7850597d52e6e673229037
Adam Semenenko <adam.semenenko@gmail.com>
2018-12-21 12:54:09 +0100

    'init'

---
+++ src/main/java/com/adam/mobiquity_package_challenge/App.java
@@ -0,0 +1,13 @@
+package com.adam.mobiquity_package_challenge;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
