bc562663d6a3d7d688dd59840796a67c9f8ff723
Adam Semenenko <adam.semenenko@gmail.com>
2018-12-23 12:41:25 +0100

    Create lots of integration tests for Parsing service

---
+++ src/test/java/test/com/mobiquityinc/packer/service/ParsingServiceTest.java
@@ -0,0 +1,202 @@
+package test.com.mobiquityinc.packer.service;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.mobiquityinc.exception.APIException;
+import com.mobiquityinc.packer.Packer;
+
+public class ParsingServiceTest {
+
+	private static File file;
+
+	@Before
+	public void beforeEachTest() throws IOException {
+		file = File.createTempFile("test", ".dat");
+		file.deleteOnExit();
+	}
+
+	@After
+	public void afterEachTest() {
+		file.delete();
+	}
+
+	@Test
+	public void testMultipleLines() throws IOException {
+
+		FileWriter writer = new FileWriter(file);
+
+		// all should fit
+		// solution: 1,2,3,4
+		writer.write("10 : (1,1,10) (2,1,10) (3,1,10) (4,1,10)\n");
+		// Test recurring numbers. 3 will fit exactly, one won't
+		// solution: 1,2,3
+		writer.write("10 : (1,3.33333,10) (2,3.33333,10) (3,3.33333,10) (4,9,10)\n");
+		// none fit
+		// solution: -
+		writer.write("0.9999999999 : (1,2.22,10) (2,3.41,10) (3,3.512,10) (4,4.5151,10)\n");
+		// test same weight, different costs
+		// solution: 2
+		writer.write("20.05 : (1,20.05,10) (2,20.05,11)\n");
+
+		writer.close();
+
+		String solutionString = Packer.pack(file.getAbsolutePath());
+
+		assertNotNull(solutionString);
+		assertFalse(solutionString.trim().isEmpty());
+
+		String[] sol = solutionString.split("\n");
+
+		assertNotNull(sol);
+		assertEquals(4, sol.length, "There are 4 inputs, so 4 solutions expected");
+
+		assertEquals("1,2,3,4", sol[0]);
+		assertEquals("1,2,3", sol[1]);
+		assertEquals("-", sol[2]);
+		assertEquals("2", sol[3]);
+
+	}
+
+	@Test
+	public void testAllFit() throws IOException {
+
+		FileWriter writer = new FileWriter(file);
+
+		// all should fit
+		// solution: 1,2,3,4
+		writer.write("10 : (1,1,10) (2,1,10) (3,1,10) (4,1,10)\n");
+
+		writer.close();
+
+		String solutionString = Packer.pack(file.getAbsolutePath());
+
+		assertNotNull(solutionString);
+		assertFalse(solutionString.trim().isEmpty());
+
+		String[] sol = solutionString.split("\n");
+
+		assertNotNull(sol);
+		assertEquals(1, sol.length, "There is 1 inputs, so 1 solution expected");
+
+		assertEquals("1,2,3,4", sol[0]);
+	}
+
+	@Test
+	public void testDecimalPlaces() throws IOException {
+
+		FileWriter writer = new FileWriter(file);
+
+		// Test recurring numbers. 3 will fit exactly, one won't
+		// solution: 1,2,3
+		writer.write("10 : (1,3.33333,10) (2,3.33333,10) (3,3.33333,10) (4,9,10)\n");
+
+		writer.close();
+
+		String solutionString = Packer.pack(file.getAbsolutePath());
+
+		assertNotNull(solutionString);
+		assertFalse(solutionString.trim().isEmpty());
+
+		assertEquals("1,2,3", solutionString);
+
+	}
+
+	@Test
+	public void testNoneFit() throws IOException {
+
+		FileWriter writer = new FileWriter(file);
+
+		// none fit
+		// solution: -
+		writer.write("1 : (1,2.22,10) (2,3.41,10) (3,3.512,10) (4,4.5151,10)\n");
+
+		writer.close();
+
+		String solutionString = Packer.pack(file.getAbsolutePath());
+
+		assertNotNull(solutionString);
+		assertFalse(solutionString.trim().isEmpty());
+
+		assertEquals("-", solutionString);
+
+	}
+
+	@Test
+	public void testSameWeightDifferentCosts() throws IOException {
+
+		FileWriter writer = new FileWriter(file);
+
+		// test same weight, different costs
+		// solution: 2
+		writer.write("20.05 : (1,20.05,10) (2,20.05,11)\n");
+
+		writer.close();
+
+		String solutionString = Packer.pack(file.getAbsolutePath());
+
+		assertNotNull(solutionString);
+		assertFalse(solutionString.trim().isEmpty());
+
+		assertEquals("2", solutionString);
+	}
+
+	@Test
+	public void testExactFitDecimals() throws IOException {
+
+		FileWriter writer = new FileWriter(file);
+
+		// test same weight, different costs
+		// solution: 2
+		writer.write("23.41 : (1,11.705,10) (2,11.705,11)\n");
+
+		writer.close();
+
+		String solutionString = Packer.pack(file.getAbsolutePath());
+
+		assertNotNull(solutionString);
+		assertFalse(solutionString.trim().isEmpty());
+
+		assertEquals("1,2", solutionString);
+	}
+
+	@Test(expected = APIException.class)
+	public void testEmptyInput() throws IOException {
+
+		FileWriter writer = new FileWriter(file);
+
+		writer.write(" \n ");
+
+		writer.close();
+
+		String solutionString = Packer.pack(file.getAbsolutePath());
+
+		assert false : "Exception should be thrown, as input file is blank. Got output: '" + solutionString + "'";
+	}
+
+	@Test
+	public void testInvalidInput_endsEarly() throws IOException {
+
+		FileWriter writer = new FileWriter(file);
+
+		// test same weight, different costs
+		// solution: 2
+		writer.write("23.41 : (1,11.70");
+
+		writer.close();
+
+		String solutionString = Packer.pack(file.getAbsolutePath());
+
+		assert false : "Exception should be thrown, as input file is blank. Got output: '" + solutionString + "'";
+	}
+
+}
