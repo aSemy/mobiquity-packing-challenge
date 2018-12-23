dd8379f7255c01030b0ad65417afbbbb0c7365ec
Adam Semenenko <adam.semenenko@gmail.com>
2018-12-23 16:40:29 +0100

    remove unused imports

--- src/main/java/com/mobiquityinc/packer/service/PackingService.java
+++ src/main/java/com/mobiquityinc/packer/service/PackingService.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
