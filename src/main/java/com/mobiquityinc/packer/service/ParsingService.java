fed264c32ae3ec587da1857f9ef7a610bba7c597
Adam Semenenko <adam.semenenko@gmail.com>
2018-12-24 02:28:13 +0100

    Create more strict parser

--- src/main/java/com/mobiquityinc/packer/service/ParsingService.java
+++ src/main/java/com/mobiquityinc/packer/service/ParsingService.java
@@ -4,13 +4,18 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.mobiquityinc.exception.APIException;
 import com.mobiquityinc.packer.model.Item;
 import com.mobiquityinc.packer.model.Parcel;
 
@@ -18,83 +23,165 @@ public class ParsingService {
 
 	private static final Logger logger = LoggerFactory.getLogger(ParsingService.class);
 
+	private static final String regexDouble = "-?\\d*\\.?\\d+";
+	private static final String regexLong = "\\d+";
+
 	public TreeMap<Parcel, ArrayList<Item>> parseFile(File file) throws FileNotFoundException, InputMismatchException {
 
-		TreeMap<Parcel, ArrayList<Item>> parsedInput = new TreeMap<>();
+		try {
 
-		Scanner fileScanner = new Scanner(file);
+			TreeMap<Parcel, ArrayList<Item>> parsedInput = new TreeMap<>();
 
-		if (logger.isDebugEnabled()) {
-			logger.debug("Reading in file " + file.getName());
+			Scanner fileScanner = new Scanner(file);
+
+			if (logger.isDebugEnabled()) {
+				logger.debug("Reading in file " + file.getName());
+				while (fileScanner.hasNextLine()) {
+					logger.debug(fileScanner.nextLine());
+				}
+				fileScanner.close();
+				fileScanner = new Scanner(file);
+			}
+
+			long lineNumber = 1;
 			while (fileScanner.hasNextLine()) {
-				logger.debug(fileScanner.nextLine());
+
+				String line = fileScanner.nextLine();
+
+				String[] lineSplit = line.split(" :");
+
+				if (lineSplit.length != 2) {
+					fileScanner.close();
+					throw new IllegalArgumentException(
+							"Invalid data format. Expected one ' :' per line, but got " + (lineSplit.length - 1));
+				}
+				if (lineSplit[0] == null || lineSplit[0].trim().isEmpty()) {
+					fileScanner.close();
+					throw new IllegalArgumentException(
+							"Invalid data format. Found no parcel weight on line " + lineNumber);
+				}
+
+				if (lineSplit[1] == null || lineSplit[1].trim().isEmpty()) {
+					fileScanner.close();
+					throw new IllegalArgumentException("Invalid data format. Found no items on line " + lineNumber);
+				}
+
+				// get the parcel weight from the first part of lineSplit
+				BigDecimal parcelWeight;
+				try {
+					parcelWeight = new BigDecimal(lineSplit[0]);
+				} catch (NumberFormatException e) {
+					fileScanner.close();
+					throw new IllegalArgumentException(
+							"Invalid data format. Could not parse parcelWeight on line " + lineNumber);
+				}
+				// now create the parcel.
+				Parcel parcel = new Parcel(lineNumber++, parcelWeight);
+
+				// now use the rest of the split to make items
+
+				String itemsString = lineSplit[1];
+
+				// validate all brackets open and close
+				validateItemBrackets(itemsString);
+
+				// match contents of brackets
+				// Pattern pattern = Pattern.compile("(
+				// \\(\\d+,\\d+\\.?\\d*,€\\d+\\.?\\d*\\))");
+				Pattern pattern = Pattern
+						.compile("( \\(" + regexLong + "," + regexDouble + ",€" + regexDouble + "\\))");
+				Matcher matcher = pattern.matcher(itemsString);
+
+				ArrayList<String> matches = new ArrayList<>();
+				while (matcher.find()) {
+					String match = matcher.group();
+					matches.add(match);
+				}
+
+				assert itemsString.equals(
+						matches.stream().collect(Collectors.joining())) : "Couldn't fully parse items. Expected '"
+								+ itemsString + "' but got '" + matches.stream().collect(Collectors.joining()) + "'";
+
+				ArrayList<Item> items = new ArrayList<>(
+						matches.stream().map(m -> parseItem(m)).collect(Collectors.toList()));
+				Collections.sort(items);
+
+				parsedInput.put(parcel, items);
+
 			}
 			fileScanner.close();
-			fileScanner = new Scanner(file);
+
+			return parsedInput;
+
+		} catch (FileNotFoundException | IllegalStateException | IllegalArgumentException | AssertionError e) {
+			throw new APIException(e);
 		}
-
-		long lineNumber = 1;
-		while (fileScanner.hasNextLine()) {
-			Scanner lineScanner = new Scanner(fileScanner.nextLine());
-			lineScanner.useDelimiter("[ ,\\(\\):€]+");
-
-			// get the parcel
-			BigDecimal bd = lineScanner.nextBigDecimal();
-
-			Parcel parcel = new Parcel(lineNumber++, bd);
-
-			parsedInput.put(parcel, new ArrayList<>());
-
-			while (lineScanner.hasNext()) {
-
-				Item item = parseItem(lineScanner);
-
-				parsedInput.get(parcel).add(item);
-
-			}
-
-			lineScanner.close();
-		}
-		fileScanner.close();
-
-		return parsedInput;
 	}
 
-	public Item parseItem(final Scanner lineScanner) {
+	public Item parseItem(final String itemString) {
+
+		Pattern itemPattern = Pattern
+				.compile(" \\((" + regexLong + "),(" + regexDouble + "),€(" + regexDouble + ")\\)");
+		Matcher matcher = itemPattern.matcher(itemString);
+
+		assert matcher.find() : "Couldn't parse item";
+		assert matcher.groupCount() == 3 : "Expect each item to have three qualities";
+
+		String indexString = matcher.group(1);
+		String weightString = matcher.group(2);
+		String costInEurosString = matcher.group(3);
 
 		long index;
 		BigDecimal weight;
 		long costInEuros;
 
-		if (lineScanner.hasNextLong()) {
-			index = lineScanner.nextLong();
-		} else {
-			InputMismatchException e = new InputMismatchException("Couldn't find index. Found: " + lineScanner.next());
-
-			lineScanner.close();
-			throw e;
+		try {
+			index = Long.parseLong(indexString);
+		} catch (NumberFormatException e) {
+			throw new NumberFormatException("Couldn't parse item index from '" + indexString + "'");
 		}
-		if (lineScanner.hasNextBigDecimal()) {
-			weight = lineScanner.nextBigDecimal();
-		} else {
-			InputMismatchException e = new InputMismatchException("Couldn't find weight. Found: " + lineScanner.next());
-
-			lineScanner.close();
-			throw e;
+		try {
+			weight = new BigDecimal(weightString);
+		} catch (NumberFormatException e) {
+			throw new NumberFormatException("Couldn't parse item weight from '" + weightString + "'");
 		}
-		if (lineScanner.hasNextLong()) {
-			costInEuros = lineScanner.nextLong();
-		} else {
-			InputMismatchException e = new InputMismatchException(
-					"Couldn't find costInEuros. Found: " + lineScanner.next());
-
-			lineScanner.close();
-			throw e;
+		try {
+			costInEuros = Long.parseLong(costInEurosString);
+		} catch (NumberFormatException e) {
+			throw new NumberFormatException("Couldn't parse item costInEuros from '" + costInEurosString + "'");
 		}
+
+		assert !matcher.find() : "There shouldn't be anything further to match";
 
 		long costInCents = costInEuros * 100;
 
 		return new Item(index, weight, costInCents);
+	}
+
+	/**
+	 * Check there's the right amount of open and close brackets
+	 * 
+	 * @param itemsString
+	 */
+	private void validateItemBrackets(final String itemsString) {
+
+		boolean bracketsOpen = false;
+		for (char c : itemsString.toCharArray()) {
+			if (c == '(') {
+				if (bracketsOpen)
+					throw new IllegalStateException("Invalid brackets, brackets opened twice");
+
+				bracketsOpen = true;
+			} else if (c == ')') {
+				if (bracketsOpen == false)
+					throw new IllegalStateException("Invalid brackets, brackets already closed");
+
+				bracketsOpen = false;
+			}
+		}
+		if (bracketsOpen == true)
+			throw new IllegalStateException("Invalid brackets, bracket never closed");
+
 	}
 
 }
